8447eef502b1c433918c38eb4a4d9997f7374836
JEAN-CLAUDE <jc@ninjametrics.com>
2015-04-06 17:13:39 -0700

    remote ssh

--- Sonet/src/com/sonet/cli/MongoAccess.java
+++ Sonet/src/com/sonet/cli/MongoAccess.java
@@ -1,5 +1,7 @@
 package com.sonet.cli;
 
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
@@ -11,6 +13,8 @@ import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.util.Hash;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.CookieHandler;
 import java.net.UnknownHostException;
 import java.sql.ResultSet;
@@ -19,6 +23,8 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
+
+import javax.swing.JOptionPane;
 
 import net.neoremind.sshxcute.core.ConnBean;
 import net.neoremind.sshxcute.core.Result;
@@ -29,7 +35,7 @@ import net.neoremind.sshxcute.task.impl.ExecCommand;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class MongoAccess {
-	public DB db =null;
+	public DB db = null;
 	public MongoClient mongoClient;
 	
 	public boolean connect(String dbname) {
@@ -37,7 +43,8 @@ public class MongoAccess {
 		try {
 //			MongoClient mongoClient = new MongoClient();
 			// or
-			mongoClient = new MongoClient( "muon-3.local" , 27017);
+//			mongoClient = new MongoClient( "quanta.local" , 27017);
+			mongoClient = new MongoClient( "localhost" , 27017);
 			// or
 //			MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
 			// or, to connect to a replica set, with auto-discovery of the primary, supply a seed list of members
@@ -64,16 +71,52 @@ public class MongoAccess {
 		int state = 1;
 		JSch jsch = new JSch();
 		try {
-//			jsch.addIdentity("/Users/jclaudel/.ssh/id_dsa");
-			Session session = jsch.getSession("gse", "sv2lxgsed03.corp.equinix.com", 22);
-			session.setPassword("welcome1");
+	        jsch.setKnownHosts("/Users/jclaudel/.ssh/known_hosts");
+	        jsch.addIdentity("/Users/jclaudel/.ssh/id_rsa");
+			Session session = jsch.getSession("jclaudel", "quanta.local", 22);
+			session.setPassword("operacom");
 //			session.setConfig("StrictHostKeyChecking", "no");
 //			java.util.Properties config = new java.util.Properties(); 
 //			config.put("StrictHostKeyChecking", "no");
 //			session.setConfig(config);			
 			session.connect();
+			// execute command
+			String cmd = "ps -elf | grep 'mongod' | grep -v grep";
+			cmd = "ps ax | grep mongod";
+//			String command=JOptionPane.showInputDialog("Enter command", 
+//                    "set|grep SSH");			
+			String command=JOptionPane.showInputDialog("Enter command", 
+                    cmd);			
+		    Channel channel=(Channel) session.openChannel("exec");
+		    ((ChannelExec)channel).setCommand(command);
+		    
+		    ((ChannelExec)channel).setErrStream(System.err);
+		    
+		    channel.setInputStream(null);
+//		    channel.setOutputStream(System.out);
+		    
+		    InputStream in=channel.getInputStream();
+		      
+		    channel.connect();
+		 
+		    byte[] tmp = new byte[1024];
+		    while(true) {
+		    		while(in.available() > 0){
+		        int i=in.read(tmp, 0, 1024);
+		        if(i < 0) break;
+		        		System.out.print(new String(tmp, 0, i));
+		        }
+		        if(channel.isClosed()){
+		        		if(in.available()>0) continue; 
+		        		System.out.println("exit-status: "+ channel.getExitStatus());
+		        		break;
+		        }
+		        try{Thread.sleep(1000);}catch(Exception ee){}		      
+		    	}
+		    channel.disconnect();
+		    session.disconnect();		    
 			state = 0;
-		} catch (JSchException e) {
+		} catch (JSchException | IOException e) {
 			e.printStackTrace();
 		}
 		return state;
@@ -82,11 +125,8 @@ public class MongoAccess {
 	public int checkMongoServer() {
 		int state = 1;
 		
-//		String ssh_host = "sv2lxgsed03.corp.equinix.com";
-		String ssh_host = "172.16.209.117";
-//		String ssh_user = "gse";
+		String ssh_host = "localhost";
 		String ssh_user = "jclaudel";
-//		String ssh_password = "welcome1";
 		String ssh_password = "operacom";
 	    ConnBean cb = new ConnBean(ssh_host, ssh_user, ssh_password);
 	    
@@ -156,11 +196,11 @@ public class MongoAccess {
 
 	public static void main(String[] args) {
 		MongoAccess mongo = new MongoAccess();
-		
-		if (mongo.checkMongoServer() != 0) {
-			System.err.println("...server not running!");
-			System.exit(101);
-		}
+		System.out.println("=== ssh state: " + mongo.checkMongod());
+//		if (mongo.checkMongoServer() != 0) {
+//			System.err.println("...server not running!");
+//			System.exit(101);
+//		}
 		
 		if (!mongo.connect("sonet"))
 			System.exit(105);
